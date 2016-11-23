16f3acccc0f2aae949d3af1a2b101afce6d58b8d
Tudge101 <jt14833@my.bristol.ac.uk>
2016-11-23 19:13:57 +0000

    Postscaler.c
    Trying to see if we can implement variable timer postscalers to enable interrupts only when required.

---
+++ Postscaler.c
@@ -0,0 +1,34 @@
+void TIMER0_IRQHandler(void)
+{
+    unsigned int isrMask;
+
+    isrMask = LPC_TIM0->IR;
+    LPC_TIM0->IR = isrMask; // Clear the Interrupt Bit
+
+    // This interrupt will be called every timer0InterruptLength ms = 5ms
+    if(isPlaying == 1){ // isPlaying gets set to 0 in Tune_StopSong();
+		if(pauseTimer >= CurrentPause){ // When the timer has exceeded CurrentPause enter this if statement
+			pauseTimer = 0; // Reset timer
+
+			if (*SongStringPointer == 0) { Tune_StopSong(); return; } // If last character in the SongStringPointer, exit the while loop
+			CurrentNote = GetNote(*SongStringPointer++);
+      LPC_TIM2->MR0  = MiliToMicroSec(CurrentNote); /* Reload timer2 value to generate interrupt after current note delay */
+
+			if (*SongStringPointer == 0) { Tune_StopSong(); return; }
+			CurrentDuration = GetDuration(*SongStringPointer++);
+      LPC_TIM1->MR0  = MiliToMicroSec(CurrentDuration); /* Reload timer1 value to generate interrupt after current duration delay */
+
+			if (*SongStringPointer == 0) { Tune_StopSong(); return; }
+			CurrentPause = GetPause(*SongStringPointer++);
+      LPC_TIM0->MR0  = MiliToMicroSec(CurrentPause); /* Reload timer0 value to generate interrupt after current pause delay */
+
+			NVIC_EnableIRQ(TIMER1_IRQn); // Enable Timer1 Interrupt
+			NVIC_DisableIRQ(TIMER0_IRQn); // Disable (current) Timer0 Interrupt
+			isPlayingNote = 1;
+
+			// Wait delay
+			// DelayMS(CurrentPause); *Removed*
+		}
+
+		pauseTimer += timer0InterruptLength; // Add 5ms to the current timer
+    }
