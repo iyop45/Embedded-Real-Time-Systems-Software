fba2bcbe5c657a964171e731a308bb310e4d0567
Tudge101 <jt14833@my.bristol.ac.uk>
2016-11-23 19:39:22 +0000

    Update Postscaler.c

--- Postscaler.c
+++ Postscaler.c
@@ -12,14 +12,17 @@ void TIMER0_IRQHandler(void)
 
 			if (*SongStringPointer == 0) { Tune_StopSong(); return; } // If last character in the SongStringPointer, exit the while loop
 			CurrentNote = GetNote(*SongStringPointer++);
+ 	/* Add timer2 reset*/
       LPC_TIM2->MR0  = MiliToMicroSec(CurrentNote); /* Reload timer2 value to generate interrupt after current note delay */
 
 			if (*SongStringPointer == 0) { Tune_StopSong(); return; }
 			CurrentDuration = GetDuration(*SongStringPointer++);
+	/* Add timer1 reset*/
       LPC_TIM1->MR0  = MiliToMicroSec(CurrentDuration); /* Reload timer1 value to generate interrupt after current duration delay */
 
 			if (*SongStringPointer == 0) { Tune_StopSong(); return; }
 			CurrentPause = GetPause(*SongStringPointer++);
+	/* Add timer0 reset*/
       LPC_TIM0->MR0  = MiliToMicroSec(CurrentPause); /* Reload timer0 value to generate interrupt after current pause delay */
 
 			NVIC_EnableIRQ(TIMER1_IRQn); // Enable Timer1 Interrupt
